a38ff115272b6b737114a5a987bf56bc601f7fe6
jeff <46485515+jeff-tooke@users.noreply.github.com>
2025-05-11 15:02:16 +0100

    modify colors for outdated packages

--- dot_config/sketchybar/plugins/executable_brew.sh
+++ dot_config/sketchybar/plugins/executable_brew.sh
@@ -7,15 +7,9 @@ COUNT="$(brew outdated | wc -l | tr -d ' ')"
 COLOR=$RED
 
 case "$COUNT" in
-  [3-5][0-9]) COLOR=$ORANGE
-  ;;
-  [1-2][0-9]) COLOR=$YELLOW
-  ;;
-  [1-9]) COLOR=$WHITE
-  ;;
-  0) COLOR=$GREEN
-     COUNT=􀆅
-  ;;
+  [1-9]|1[0-2]) COLOR=$YELLOW ;;
+  1[3-9]|2[0-5]) COLOR=$YELLOW ;;
+  0) COLOR=$GREEN; COUNT=􀆅 ;;
 esac
 
 sketchybar --set $NAME label=$COUNT icon.color=$COLOR
