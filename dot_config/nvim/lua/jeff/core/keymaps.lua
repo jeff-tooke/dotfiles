a9802184308a11fc289b5de4784eef0303f5d112
jeff <46485515+jeff-tooke@users.noreply.github.com>
2025-05-11 12:36:26 +0100

    update nvim keymaps

--- dot_config/nvim/lua/jeff/core/keymaps.lua
+++ dot_config/nvim/lua/jeff/core/keymaps.lua
@@ -2,21 +2,17 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("n", "jj", "i", { desc = "Enter insert mode with jj" })
+-- Optimise switching between modes
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
+keymap.set("n", "jk", "i", { desc = "Enter insert mode with jk" })
 keymap.set("n", ";", ":", { desc = "Enter command mode with ;" })
---keymap.set("n", ":", ";", { desc = "Enter command mode with ;" })
 
 -- Get that muscle memory going;
 keymap.set({ 'n', 'v' }, '<Up>', function() print("Use 'k' instead 🤓") end, opts)
 keymap.set({ 'n', 'v' }, '<Down>', function() print("Use 'j' instead 🤓") end, opts)
 keymap.set({ 'n', 'v' }, '<Left>', function() print("Use 'h' instead 🤓") end, opts)
 keymap.set({ 'n', 'v' }, '<Right>', function() print("Use 'l' instead 🤓") end, opts)
---keymap.set('i', '<Up>', function() print("Use 'k' instead 🤓") end, opts)
---keymap.set('i', '<Down>', function() print("Use 'j' instead 🤓") end, opts)
---keymap.set('i', '<Left>', function() print("Use 'h' instead 🤓") end, opts)
---keymap.set('i', '<Right>', function() print("Use 'l' instead 🤓") end, opts)
 
-
+-- Custom stuff
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
