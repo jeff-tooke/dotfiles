02177ff87a218ae091c16a52119854c7eb92c6f9
jeff <46485515+jeff-tooke@users.noreply.github.com>
2025-05-14 13:54:47 +0100

    modify buffer line

--- dot_config/nvim/lua/jeff/plugins/bufferline.lua
+++ dot_config/nvim/lua/jeff/plugins/bufferline.lua
@@ -1,10 +1,13 @@
 return {
-	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	version = "*",
-	opts = {
-		options = {
-			mode = "buffers",
-		},
-	},
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "buffers",
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      separator_style = "slant",
+    },
+  },
 }
