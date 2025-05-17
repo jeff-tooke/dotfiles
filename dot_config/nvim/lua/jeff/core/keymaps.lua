fb0b1be9db9d9a98c1fe48fde10d83371ea6f5f9
jeff <46485515+jeff-tooke@users.noreply.github.com>
2025-05-17 23:01:39 +0100

    add keymap for terminal mode

--- dot_config/nvim/lua/jeff/core/keymaps.lua
+++ dot_config/nvim/lua/jeff/core/keymaps.lua
@@ -8,11 +8,19 @@ keymap.set("n", "jk", "i", { desc = "Enter insert mode with jk" })
 keymap.set("n", ";", ":", { desc = "Enter command mode with ;" })
 
 -- Get that muscle memory going;
-keymap.set({ 'n', 'v' }, '<Up>', function() print("Use 'k' instead 🤓") end, opts)
-keymap.set({ 'n', 'v' }, '<Down>', function() print("Use 'j' instead 🤓") end, opts)
-keymap.set({ 'n', 'v' }, '<Left>', function() print("Use 'h' instead 🤓") end, opts)
-keymap.set({ 'n', 'v' }, '<Right>', function() print("Use 'l' instead 🤓") end, opts)
+keymap.set({ "n", "v" }, "<Up>", function()
+	print("Use 'k' instead 🤓")
+end, opts)
+keymap.set({ "n", "v" }, "<Down>", function()
+	print("Use 'j' instead 🤓")
+end, opts)
+keymap.set({ "n", "v" }, "<Left>", function()
+	print("Use 'h' instead 🤓")
+end, opts)
+keymap.set({ "n", "v" }, "<Right>", function()
+	print("Use 'l' instead 🤓")
+end, opts)
 
 -- Custom stuff
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
+keymap.set("t", "<ESC>", [[<C-\><C-n>]], { desc = "Exit out of terminal mode" })
