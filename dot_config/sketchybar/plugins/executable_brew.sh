26294da6eb1ef0dd9a10441d6833f49e28cbf898
jeff <46485515+jeff-tooke@users.noreply.github.com>
2025-05-17 23:47:33 +0100

    fix case statement order

--- dot_config/sketchybar/plugins/executable_brew.sh
+++ dot_config/sketchybar/plugins/executable_brew.sh
@@ -7,10 +7,10 @@ COUNT="$(brew outdated | wc -l | tr -d ' ')"
 COLOR=$RED
 
 case "$COUNT" in
+  0) COLOR=$GREEN; COUNT=􀆅 ;;
   [1-9]) COLOR=$YELLOW ;;
   1[0-9]) COLOR=$ORANGE;;
   *) COLOR=$RED;;
-  0) COLOR=$GREEN; COUNT=􀆅 ;;
 esac
 
 sketchybar --set $NAME label=$COUNT icon.color=$COLOR
