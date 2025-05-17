5b07a6a6c020e09b380f8f96fd280eadc27b66de
jeff <46485515+jeff-tooke@users.noreply.github.com>
2025-05-17 22:50:33 +0100

    add toggleterm

--- dot_config/nvim/lua/jeff/plugins/toggleterm.lua
+++ dot_config/nvim/lua/jeff/plugins/toggleterm.lua
@@ -1,23 +1,27 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
+	"akinsho/toggleterm.nvim",
+	version = "*",
 
-  config = function()
-    local toggleterm = require "toggleterm"
+	config = function()
+		local toggleterm = require("toggleterm")
 
-    toggleterm.setup {
-      direction = "vertical",
-      start_in_insert = true,
-      close_on_exit = true,
-      shell = vim.o.shell,
-    }
+		toggleterm.setup({
+			direction = "vertical",
+			start_in_insert = true,
+			close_on_exit = true,
+			shell = vim.o.shell,
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return math.floor(vim.o.columns * 0.5)
+				end
+			end,
+		})
 
-    --set keymaps
-    local keymap = vim.keymap
+		--set keymaps
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>t", "<cmd>ToggleTerm direction=vertical<cr>", { desc = "Toggle vertical terminal" })
-    keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Toggle horizontal terminal" })
-    keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Toggle floating terminal" })
-    keymap.set("n", "<leader>tt", "<cmd>ToggleTerm direction=tab<cr>", { desc = "Toggle tabbed terminal" })
-  end,
+		keymap.set("n", "<leader>t", "<cmd>ToggleTerm direction=vertical<cr>", { desc = "Toggle vertical terminal" })
+	end,
 }
