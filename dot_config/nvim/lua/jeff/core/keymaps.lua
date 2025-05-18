ee861d9c12f2e63891d938895cc5da6230c6f57d
jeff <46485515+jeff-tooke@users.noreply.github.com>
2025-05-18 19:09:20 +0100

    add some sensible nvim keymaps

--- dot_config/nvim/lua/jeff/core/keymaps.lua
+++ dot_config/nvim/lua/jeff/core/keymaps.lua
@@ -5,7 +5,7 @@ local keymap = vim.keymap
 -- Optimise switching between modes
 keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 keymap.set("n", "jk", "i", { desc = "Enter insert mode with jk" })
-keymap.set("n", ";", ":", { desc = "Enter command mode with ;" })
+-- keymap.set("n", ";", ":", { desc = "Enter command mode with ;" })
 
 -- Get that muscle memory going;
 keymap.set({ "n", "v" }, "<Up>", function()
@@ -24,3 +24,43 @@ end, opts)
 -- Custom stuff
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("t", "<ESC>", [[<C-\><C-n>]], { desc = "Exit out of terminal mode" })
+keymap.set("n", "<leader>q", ":wqa<CR>", { noremap = true, silent = true, desc = "Save and exit all buffers" })
+keymap.set("n", "<leader>x", ":qa!<CR>", { noremap = true, silent = true, desc = "Force quit all!" })
+
+-- Track state internally
+local transparency_enabled = false
+
+local function toggle_transparency()
+	transparency_enabled = not transparency_enabled
+
+	require("catppuccin").setup({
+		transparent_background = transparency_enabled,
+	})
+
+	vim.cmd("colorscheme catppuccin")
+
+	-- Optional: clear backgrounds of other groups
+	if transparency_enabled then
+		local groups = {
+			"NormalNC",
+			"NvimTreeNormal",
+			"TelescopeNormal",
+			"TelescopeBorder",
+			"FloatBorder",
+			"Pmenu",
+			"StatusLine",
+			"StatusLineNC",
+		}
+		for _, group in ipairs(groups) do
+			vim.api.nvim_set_hl(0, group, { bg = "none" })
+		end
+	end
+
+	vim.notify(
+		"Transparency " .. (transparency_enabled and "Enabled" or "Disabled"),
+		vim.log.levels.INFO,
+		{ title = "Catppuccin" }
+	)
+end
+
+keymap.set("n", "<leader>bg", toggle_transparency, { desc = "Toggle background transparency" })
