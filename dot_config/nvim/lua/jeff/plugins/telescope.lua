f788fc9d0a9417cbb3e22eea4b1c60a3f36c0c17
jeff <46485515+jeff.tooke@users.noreply.github.com>
2025-08-23 14:26:24 +0100

    add oil and telescope

---
+++ dot_config/nvim/lua/jeff/plugins/telescope.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      local telescope = require "telescope"
+      telescope.setup {
+        defaults = {
+          path_display = { "smart" },
+          preview = false,
+          layout_strategy = "center",
+          layout_config = {
+            width = 0.4,
+            height = 0.6,
+          },
+        },
+      }
+
+      telescope.load_extension "fzf"
+
+      local keymap = vim.keymap
+
+      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files in current directory" })
+      keymap.set("n", "<leader>fc", function()
+        require("telescope.builtin").find_files { cwd = vim.fn.stdpath "config" }
+      end, { desc = "Find config file" })
+      keymap.set("n", "<leader><space>", "<cmd>Telescope find_files<cr>", { desc = "Find files in current directory" })
+      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
+      keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
+      keymap.set("n", "<leader>/", "<cmd>Telescope live_grep<cr>", { desc = "Find string in current directory" })
+      keymap.set("n", "<leader>,", "<cmd>Telescope buffers<cr>", { desc = "Find open buffers" })
+      keymap.set("n", "<leader>:", "<cmd>Telescope command_history<cr>", { desc = "Find recently executed commands" })
+      keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags <cr>", { desc = "Find help tags" })
+      keymap.set("n", "<leader>fm", "<cmd>Telescope man_pages<cr>", { desc = "Find man pages" })
+    end,
+  },
+}
