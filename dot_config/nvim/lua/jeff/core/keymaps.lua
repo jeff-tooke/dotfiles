e46d30ffd3fb55aaa69bccd3089217e8f54d95e6
jeff <46485515+jeff.tooke@users.noreply.github.com>
2025-08-23 14:25:53 +0100

    Update keymaps

--- dot_config/nvim/lua/jeff/core/keymaps.lua
+++ dot_config/nvim/lua/jeff/core/keymaps.lua
@@ -11,19 +11,12 @@ keymap.set({ "n", "v" }, "<Up>", "<Nop>", opts)
 keymap.set({ "n", "v" }, "<Down>", "<Nop>", opts)
 keymap.set({ "n", "v" }, "<Left>", "<Nop>", opts)
 keymap.set({ "n", "v" }, "<Right>", "<Nop>", opts)
-keymap.set("i", "<Up>", '<Esc>:echo "Use k"<CR>a', opts)
-keymap.set("i", "<Down>", '<Esc>:echo "Use j"<CR>a', opts)
-keymap.set("i", "<Left>", '<Esc>:echo "Use h"<CR>a', opts)
-keymap.set("i", "<Right>", '<Esc>:echo "Use l"<CR>a', opts)
 
 -- Custom stuff
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("t", "<ESC>", [[<C-\><C-n>]], { desc = "Exit out of terminal mode" })
-keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true, desc = "Save buffer" })
+keymap.set("n", "<leader>e", ":Oil<CR>", { desc = "Open file explorer" })
+keymap.set("n", "<leader>w", ":up<CR>", { noremap = true, silent = true, desc = "Save buffer" })
 keymap.set("n", "<leader>q", ":wqa<CR>", { noremap = true, silent = true, desc = "Save and exit all buffers" })
-keymap.set("n", "<leader>x", ":qa!<CR>", { noremap = true, silent = true, desc = "Force quit all!" })
-keymap.set("n", "<leader><left>", ":bp<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
-keymap.set("n", "<leader><right>", ":bn<CR>", { noremap = true, silent = true, desc = "Next buffer" })
 keymap.set("n", "<leader>ca", ":up <bar> %bd <bar> e# <bar> bd# <CR>", { noremap = true, silent = true, desc = "Update and close all buffers except current" })
 
 -- Track state internally
