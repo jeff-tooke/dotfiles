30145434aaf856080d460d3b02c89a3afaa762a4
Jeff Tooke <tooke.jeff@gmail.com>
2026-06-05 14:07:25 +0100

    nix config file

---
+++ linux/nixos/configuration.nix
@@ -0,0 +1,89 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos";
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "uk";
+  services.xserver.xkb.layout = "gb";
+
+  # Define a user account.
+  users.users.jeff = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "changeme";
+    shell = pkgs.zsh;
+  };
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [ neovim git wget foot wl-clipboard wofi waybar dunst hyprpaper slurp grim swaylock greetd tuigreet];
+
+  programs.hyprland.enable = true;
+  programs.zsh.enable = true;
+
+  fonts.packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+  ];
+
+  # Enabled services
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+
+  services.greetd.enable = true;
+  services.greetd.settings.default_session = {
+     command = "tuigreet --time --remember --cmd start-hyprland";
+     user = "jeff";
+  };
+
+  # Turn on experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
+
